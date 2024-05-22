0e84db07a9ed6b787321d325b73e1574c07607b1
Noor Khan <noordotkhan@gmail.com>
2024-05-21 22:14:41 -0400

    pew

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { Button } from "$lib/components/ui/button";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>Click <a href="test">here</a> for other route</p>
+
+<Button>Random button</Button>
+
+<style lang="scss">
+    a {
+        text-decoration: underline;
+    }
+
+    :global(Button) {
+        background-color: orange;
+    }
+</style>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { Button } from "$lib/components/ui/button";
+</script>
+
+<h1>Hello!</h1>
+
+<Button>another button</Button>
+
